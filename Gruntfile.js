ff12634509d4305981af1750c585ac0934a08a21
John Tsevdos <tsevdosjohn@gmail.com>
2014-01-05 14:57:01 +0200

    Minor task fixes. Improved  documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,220 +1,223 @@
-//"use strict";
+(function(){
+"use strict";
 
-module.exports = function (grunt) {
+    module.exports = function (grunt) {
 
-    /*
-        Grunt installation:
-        -------------------
-            npm install -g grunt-cli
-            npm install -g grunt-init
-            npm init (creates a `package.json` file)
+        /*
+            Grunt installation:
+            -------------------
+                npm install -g grunt-cli
+                npm install -g grunt-init
 
-        Project Dependencies:
-        ---------------------
-            npm install grunt --save-dev
-            npm install load-grunt-tasks --save-dev
-            npm install time-grunt --save-dev
-            npm install grunt-contrib-jshint --save-dev
-            npm install grunt-contrib-uglify --save-dev
-            npm install grunt-contrib-sass --save-dev
-            npm install grunt-contrib-imagemin --save-dev
-            npm install grunt-contrib-htmlmin --save-dev
-            npm install grunt-contrib-connect --save-dev
-            npm install connect-livereload --save-dev
-            npm install grunt-contrib-watch --save-dev
+            Project Dependencies:
+            ---------------------
+                npm install grunt --save-dev
+                npm install load-grunt-tasks --save-dev
+                npm install time-grunt --save-dev
+                npm install grunt-contrib-jshint --save-dev
+                npm install grunt-contrib-uglify --save-dev
+                npm install grunt-contrib-sass --save-dev
+                npm install grunt-contrib-imagemin --save-dev
+                npm install grunt-contrib-htmlmin --save-dev
+                npm install grunt-contrib-connect --save-dev
+                npm install connect-livereload --save-dev
+                npm install grunt-contrib-watch --save-dev
+                npm install grunt-open --save-dev
+                npm install node-bourbon --save-dev
 
-        Simple Dependency Install:
-        --------------------------
-            npm install (from the same root directory as the `package.json` file)
+            Simple Dependency Install:
+            --------------------------
+                npm install (from the same root directory as the `package.json` file)
 
-        Gem Dependencies:
-        -----------------
-            gem install image_optim
-    */
+            Gem Dependencies:
+            -----------------
+                gem install image_optim
 
-    // show elapsed time at the end
-    require('time-grunt')(grunt);
+        */
 
-    // load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+        // show elapsed time at the end
+        require('time-grunt')(grunt);
 
-    // Project configuration.
-    grunt.initConfig({
+        // load all grunt tasks
+        require('load-grunt-tasks')(grunt);
 
-        // Store your Package file so you can reference its specific data whenever necessary
-        pkg: grunt.file.readJSON('package.json'),
+        // Project configuration.
+        grunt.initConfig({
 
-        // configurable paths
-        paths: {
-            app: './app',
-            dist: './public'
-        },
+            // Store your Package file so you can reference its specific data whenever necessary
+            pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            files: ['Gruntfile.js',  '<%= paths.app %>/**/*.js'],
-            options: {
-                curly:   true,
-                eqeqeq:  true,
-                immed:   true,
-                latedef: true,
-                newcap:  true,
-                noarg:   true,
-                sub:     true,
-                undef:   true,
-                boss:    true,
-                eqnull:  true,
-                browser: true,
+            // configurable paths
+            paths: {
+                app: './app',
+                dist: './public'
+            },
 
-                globals: {
-                    // AMD
-                    module:     true,
-                    require:    true,
-                    requirejs:  true,
-                    define:     true,
+            jshint: {
+                files: ['Gruntfile.js',  '<%= paths.app %>/**/*.js'],
+                options: {
+                    curly:   true,
+                    eqeqeq:  true,
+                    immed:   true,
+                    latedef: true,
+                    newcap:  true,
+                    noarg:   true,
+                    sub:     true,
+                    undef:   true,
+                    boss:    true,
+                    eqnull:  true,
+                    browser: true,
 
-                    // Environments
-                    console:    true,
+                    globals: {
+                        // AMD
+                        module:     true,
+                        require:    true,
+                        requirejs:  true,
+                        define:     true,
+                        // Environments
+                        console:    true,
+                        // General Purpose Libraries
+                        $:          true,
+                        jQuery:     true,
 
-                    // General Purpose Libraries
-                    $:          true,
-                    jQuery:     true,
+                        // Testing
+                        sinon:      true,
+                        describe:   true,
+                        it:         true,
+                        expect:     true,
+                        beforeEach: true,
+                        afterEach:  true
+                    }
+                }
+            },
 
-                    // Testing
-                    sinon:      true,
-                    describe:   true,
-                    it:         true,
-                    expect:     true,
-                    beforeEach: true,
-                    afterEach:  true
+            uglify: {
+                my_target: {
+                    files: [{
+                        expand: true,
+                        cwd: '<%= paths.app %>/js',
+                        src: '**/*.js',
+                        dest: './public/js'
+                    }]
+                }
+            },
+
+            sass: {
+                dev: {
+                    options: {
+                        style: 'expanded',
+                        compass: true,
+                        loadPath: require('node-bourbon').includePaths
+                    },
+                    files: [{
+                        expand: true,
+                        cwd: '<%= paths.app %>/scss',
+                        src: ['**/*.scss', '!**/_*.scss'],
+                        dest: '<%= paths.app %>/css',
+                        ext: '.css'
+                    }]
+                },
+                dist: {
+                    options: {
+                        style: 'compressed',
+                        compass: true,
+                        loadPath: require('node-bourbon').includePaths
+                    },
+                    files: [{
+                        expand: true,
+                        cwd: '<%= paths.app %>/scss',
+                        src: ['**/*.scss', '!**/_*.scss'],
+                        dest: '<%= paths.dist %>/css',
+                        ext: '.css'
+                    }]
+                }
+            },
+
+            imagemin: {
+                dynamic: {
+                    files: [{
+                        expand: true,
+                        cwd: '<%= paths.app %>/img',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: '<%= paths.dist %>/img'
+                    }]
+                }
+            },
+
+            htmlmin: {
+                dist: {
+                    options: {
+                        removeComments: true,
+                        collapseWhitespace: true
+                    },
+                    files: [{
+                        expand: true,
+                        dot: false,
+                        cwd: '<%= paths.app %>',
+                        dest: '<%= paths.dist %>',
+                        src: ['*.html']
+                    }]
+                }
+            },
+
+            watch: {
+                css: {
+                    files: ['Gruntfile.js', 'app/**/*.js', './app/scss/**/*.scss'],
+                    tasks: ['dev']
+                },
+                livereload: {
+                    options: {
+                        livereload: true
+                    },
+                    files: ['<%= paths.app %>/**/*.js', '<%= paths.app %>/**/*.html', '<%= paths.app %>/css/**/*.css']
+                }
+            },
+
+            connect: {
+                server: {
+                    options: {
+                        port: 8000,
+                        base: './app',
+                        livereload: true
+                    }
+                }
+            },
+            
+            open: {
+                dev : {
+                    path: 'http://localhost:8000'
                 }
             }
-        },
+            
+        });
 
-        uglify: {
-            my_target: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= paths.app %>/js',
-                    src: '**/*.js',
-                    dest: './public/js'
-                }]
-            }
-        },
+        // Default Task
+        grunt.registerTask('default', [
+            'htmlmin',
+            'sass:dist',
+            'uglify',
+            'imagemin'
+        ]);
 
-        sass: {
-            dev: {
-                options: {
-                    style: 'expanded',
-					compass: true,
-					loadPath: require('node-bourbon').includePaths
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= paths.app %>/scss',
-                    src: ['**/*.scss', '!**/_*.scss'],
-                    dest: '<%= paths.app %>/css',
-                    ext: '.css'
-                }]
-            },
-            dist: {
-                options: {
-                    style: 'compressed',
-					compass: true,
-					loadPath: require('node-bourbon').includePaths
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= paths.app %>/scss',
-					src: ['**/*.scss', '!**/_*.scss'],
-                    dest: '<%= paths.dist %>/css',
-                    ext: '.css'
-                }]
-            }
-        },
-
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= paths.app %>/img',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: '<%= paths.dist %>/img'
-                }]
-            }
-        },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: [{
-                    expand: true,
-                    dot: false,
-                    cwd: '<%= paths.app %>',
-                    dest: '<%= paths.dist %>',
-                    src: ['*.html']
-                }]
-            }
-        },
-
-        watch: {
-            css: {
-                files: ['Gruntfile.js', 'app/**/*.js', './app/scss/**/*.scss'],
-                tasks: ['default']
-            },
-            livereload: {
-                options: {
-                    livereload: true
-                },
-                files: ['<%= paths.app %>/**/*.js', '<%= paths.app %>/**/*.html', '<%= paths.app %>/css/**/*.css']
-            }
-        },
-
-        connect: {
-            server: {
-                options: {
-                    port: 8000,
-                    base: './app',
-                    livereload: true,
-                    //keepalive: true
-                }
-            }
-        },
+        // Server Task
+         grunt.registerTask('serve', [
+            'connect',
+            'open',
+            'watch'
+        ]);
         
-		open: {
-			dev : {
-				path: 'http://localhost:8000'
-			}
-		}
-        
-    });
+        // Testing
+        grunt.registerTask('test', [
+            'jshint'
+        ]);
 
-    // Default Task
-    grunt.registerTask('default', [
-		'jshint',
-		'sass:dev',
-		'watch'
-	]);
+        // (helper task)
+        grunt.registerTask('dev', [
+            'jshint',
+            'sass:dev',
+            'watch'
+        ]);
+      
+    };
 
-    // Testing
-    grunt.registerTask('test', [
-		'jshint'
-	]);
-
-    // Release Task
-    grunt.registerTask('release', [
-		'htmlmin',
-		'sass:dist',
-		'uglify','imagemin'
-	]);
-    
-    // Server Task
-     grunt.registerTask('server', [
-		'connect',
-		'open',
-		'watch'
-	]);
-
-};
+}());
--- README.md
+++ README.md
@@ -1,41 +1,43 @@
 # Grunt boilerplate for web designers
 
-> **Grunt boilerplate for web designers** provides a nice file and folder structure to aid web-designers kickstart their next project. It will help you in the development and deployment process of any HTML-based template (and not only). Some of the features that you get out of the box are:
+**Grunt boilerplate for web designers** provides a nice directory and file structure to aid web-designers kickstart their next project. It will help you in the development and deployment process of any HTML-based template (and not only). Some of the features that you get out of the box are:
 
-- SMACSS (Scalable and Modular Architecture for CSS) ready infastructure (http://smacss.com/)
+- [SMACSS (Scalable and Modular Architecture for CSS) ](http://smacss.com/) ready infrastructure
 - Compile Sass files
-- Compass and Burbon support
+- Compass(http://compass-style.org/) and Burbon(http://bourbon.io/) support
 - JsLinter
 - CSS, JS and HTML minification
-- Image optimizasion
+- Image optimization
+- Live-reload
 
 ## Installation
 
 Follow the [Getting started with Grunt](http://gruntjs.com/getting-started) for an in-depth look at setting up Grunt, but basically:
 
 1. Boot up Terminal
-2. Make sure to have **[Node.js](http://nodejs.org/download/)** installed. (Plus Sass)
-3. You need to install Sass `gem install sass` and Image Optimization gem `gem install image_optim` (find out how to install gems here(http://guides.rubygems.org/rubygems-basics/)
+2. Make sure to have **[Node.js](http://nodejs.org/download/)** installed.
+3. You need to install Sass `gem install sass` and Image Optimization gem `gem install image_optim` (find out how to install gems [here](http://guides.rubygems.org/rubygems-basics/)
 4. Install **Grunt CLI globally** `npm install -g grunt-cli`
 5. `cd` to your project folder
-6. Git clone, or download the zip from https://github.com/tsevdos/Grunt-boilerplate-for-web-designers.git
+6. Git clone (`git clone git@github.com:tsevdos/Grunt-boilerplate-for-web-designers.git`) or download the zip from https://github.com/tsevdos/Grunt-boilerplate-for-web-designers
 7. Install Grunt and required grunt tasks `npm install`
 8. Run `grunt server` and enjoy
 
+## File structure
+
+Your "development" environment is the into the "app" directory - you do all the work there. Whenever you are ready you can "deploy" the work by running the `grunt deploy` task. This task will generate a `public` directory that will contain all your files (CSS, JS, HTML and images) optimized and ready to distribute/upload/share.
 
 ## Tasks
 
-1. grunt (default task)
-2. grunt test
-3. grunt release
-4. grunt server
+You can run the below tasks.
 
+1. `grunt` (default task) : Compiles the Sass files and makes a lints the Javascript files.
+2. `grunt serve` : Probably the most usable task. Just run this task and work elegantly with live-reload on your localhost (http://localhost:8000). The task will take care of your Sass compilation  and Live- and the provided goodies.
+3. `grunt test` : Lints the Javascript files.
+//3. `grunt deploy` : Creates a `public` directory with all the optimized files (read **File structure** section for more details).
 
-## Directories and files
-
-Your "development" environment is the in the "app" directory - you do all the work there. (Write more regarding the public folder).
 
 ## TODO:
 
-- Optimize some tasks
+- Optimize tasks
 - fix the "Port 35729 is already in use by another process"
